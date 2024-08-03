947fe57f59929f6e7a02d8c2921d78555e75e50c
Brise li <147622546+BriseLee@users.noreply.github.com>
2024-08-03 20:10:42 +0200

    __init__.py

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,10 +1,11 @@
 from .binary_signal import BinarySingal
 from .cart_kalm_filter import CartKalmanFilter
-from .game_pad_controller import GamePadTCPController, MjViewGamePadController
-from .hololens_controller import HandTrackerTCPController, HoloLensTCPController, VirtualRobotTCPController
+# from .game_pad_controller import GamePadTCPController, MjViewGamePadController
+# from .hololens_controller import HandTrackerTCPController, HoloLensTCPController, VirtualRobotTCPController
 from .kalman_filter import KalmanFilter
 from .Keyboard_Controller import KeyboardTCPController
 # from .real_robot_controller import RealRobotController
+from .Meta_Controller import MetaQuest3Controller_right
 from .replay_controller import ReplayerController
 from .tcp_controller import InteractiveTCPControllerBase
 from .vive_pro_controller import ViveProMotionControllerTCPController
